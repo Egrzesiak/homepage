4d8a192381839c8ebc93f3da2b67c261ac5bc020
Emil Grzesiak <emilgrzesiak@gmail.com>
2019-06-20 08:44:23 +0200

    feat. 5 homework e1 - js function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+function welcome(name, secondName, age, isAlert)
+{
+    let ageMessage = ``;
+    if(age<=29) ageMessage = `Chciałbym być tak młody jak TY! :-)`;
+    else if (age>29 && age <=50) ageMessage = `Fajnie jest odczuwać części ciała o których dwudziestolatek jeszcze nie ma pojęcia? :-P`;
+    else ageMessage = `Fajnie, że jesteś! :-)`;
+
+    let message = `Witaj ${name} ${secondName} na mojej stronie! ${ageMessage}`;
+
+    if(isAlert) return alert(message);
+    else console.log(message); 
+}
+
+
 console.log("Siema Eniu!");
